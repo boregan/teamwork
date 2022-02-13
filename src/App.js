385a855840e51c72d860c95cfab22cd332f9c249
Ben <117424056@umail.ucc.ie>
2022-02-13 00:18:32 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./App.scss";
+import { useEffect, useState } from "react";
+
+import People from "./components/People";
+import Header from "./components/Header";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [homeworld, setHomeworld] = useState([]);
+  const [count, setCount] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let pages = [];
+
+      // Get all pages of people from swapi.dev
+      for (let i = 1; i <= 7; i++) {
+        const response = await fetch(`https://swapi.dev/api/people/?page=${i}`);
+        const data = await response.json();
+        pages.push(data);
+      }
+
+      // Get all people from all pages
+      const people = [].concat(...pages.map((page) => page.results));
+
+      // Get all homeworlds from all people
+      const homeworlds = people.map(async (person) => {
+        const response = await fetch(person.homeworld);
+        const data = await response.json();
+        return data.name;
+      });
+
+      // Wait until all homeworlds are fetched
+      Promise.all(homeworlds).then((homeworlds) => {
+        setHomeworld(homeworlds);
+      });
+
+      // Convert all dates to readable format
+      people.forEach((person) => {
+        person.created = new Date(person.created).toDateString();
+        person.edited = new Date(person.edited).toDateString();
+      });
+
+      people.forEach((person) => {
+        if (person.mass === "unknown") {
+          person.mass = 0;
+        }
+      });
+
+      setPeople(people);
+      setCount(pages[0].count);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [people]);
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filterPeople = (people, filter) => {
+    return people.filter((person) => {
+      return person.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header filter={filter} handleFilter={handleFilter} />
+
+      {loading ? (
+        <h1 className="loading">Loading...</h1>
+      ) : (
+        <>
+          <People people={filterPeople(people, filter)} homeworld={homeworld} />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/People.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Card, Container, Grid } from "@mui/material";
+import "../App.scss";
+
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Stack } from "@mui/material";
+
+export default function People({ people, homeworld }) {
+  return (
+    <>
+      <Container>
+        <Paper
+          component={Stack}
+          direction="column"
+          justifyContent="center"
+          sx={{ width: "100%", overflow: "auto" }}
+        >
+          <TableContainer sx={{ maxHeight: "90vh" }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead className="head">
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Height (cm)</TableCell>
+                  <TableCell>Mass (kg)</TableCell>
+                  <TableCell>Created</TableCell>
+                  <TableCell>Edited</TableCell>
+                  <TableCell>Homeworld</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {people.map((person, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{person.name}</TableCell>
+                    <TableCell>{person.height}</TableCell>
+                    <TableCell>{person.mass}</TableCell>
+                    <TableCell>{person.created}</TableCell>
+                    <TableCell>{person.edited}</TableCell>
+                    <TableCell>{homeworld[index]}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Container>
+    </>
+  );
+}
